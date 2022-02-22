06665f7f43c1909c89d4d88458ca49e51417afe7
Khai Lam <khail@minerva.vn>
2022-02-22 13:39:43 +0700

    fixed console

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from 'controllers/app/store';
 import { Provider } from 'react-redux';
-// import { history } from 'utils';
 
 
 ReactDOM.render(
