06d5f3a98e6ea387c40744b5315f62634c7a72d7
lamkhai <lamkhai0604@gmail.com>
2022-05-30 16:22:05 +0700

    fixed

--- src/view/components/layout/RegisterModal/PersonalDetailsForm/index.tsx
+++ src/view/components/layout/RegisterModal/PersonalDetailsForm/index.tsx
@@ -1,14 +1,31 @@
-import { FormEvent } from 'react';
+import { FormEvent, MutableRefObject, useRef } from 'react';
 import Button from 'view/components/base/Button';
 import Divider from 'view/components/base/Divider';
 import Input from 'view/components/base/FormikField';
 import './style.css';
 
 const PersonalDetailsForm = () => {
-  const handleRegistrationSubmit = (e: FormEvent): void => {};
+  //Ref
+  const firstNameRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>;
+  const lastNameRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>;
+  const emailRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>;
+  const phoneRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>;
+  const paswordRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>;
+  const paswordConfirmRef = useRef<HTMLInputElement>(null) as MutableRefObject<HTMLInputElement>;
+  const handleRegistrationSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const formValues = {
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+      email: emailRef.current.value,
+      phone: phoneRef.current.value,
+      password: paswordRef.current.value,
+      passwordConfirm: paswordConfirmRef.current.value,
+    }
+  };
 
   return (
-    <div className="personal-detail">
+    <form className="personal-detail" onSubmit={handleRegistrationSubmit}>
       <h4>Your personal details</h4>
 
       <Divider />
@@ -16,6 +33,7 @@ const PersonalDetailsForm = () => {
       <Input
         name="firstName"
         label="First Name"
+        ref={firstNameRef}
         type="text"
         fullWidth
         sx={{ marginBottom: '1rem' }}
@@ -23,7 +41,8 @@ const PersonalDetailsForm = () => {
       />
       <Input
         label="Last Name"
-        name="Last Name"
+        name="lastName"
+        ref={lastNameRef}
         type="text"
         fullWidth
         sx={{ marginBottom: '1rem' }}
@@ -32,6 +51,7 @@ const PersonalDetailsForm = () => {
       <Input
         label="Email"
         name="email"
+        ref={emailRef}
         type="email"
         fullWidth
         sx={{ marginBottom: '1rem' }}
@@ -40,7 +60,8 @@ const PersonalDetailsForm = () => {
       <Input
         label="Phone"
         name="phone"
-        type="number"
+        ref={phoneRef}
+        type="tel"
         fullWidth
         sx={{ marginBottom: '1rem' }}
         required
@@ -50,30 +71,30 @@ const PersonalDetailsForm = () => {
 
       <Divider />
 
-      <form>
-        <Input
-          label="Password"
-          name="password"
-          type="password"
-          fullWidth
-          sx={{ marginBottom: '1rem' }}
-          required
-        />
-        <Input
-          label="Password Confirm"
-          name="password"
-          type="password"
-          fullWidth
-          sx={{ marginBottom: '1rem' }}
-          required
-        />
-        <div className="d-flex justify-content-end my-3">
-          <Button type="submit" variant="contained" size="large">
-            Continue
-          </Button>
-        </div>
-      </form>
-    </div>
+      <Input
+        label="Password"
+        name="password"
+        ref={paswordRef}
+        type="password"
+        fullWidth
+        sx={{ marginBottom: '1rem' }}
+        required
+      />
+      <Input
+        label="Password Confirm"
+        name="passwordConfirm"
+        ref={paswordConfirmRef}
+        type="password"
+        fullWidth
+        sx={{ marginBottom: '1rem' }}
+        required
+      />
+      <div className="d-flex justify-content-end my-3">
+        <Button type="submit" variant="contained" size="large">
+          Continue
+        </Button>
+      </div>
+    </form>
   );
 };
 
